ccbe1ef6e0e9b65a1944d65617c52acc96ca2d08
2017100553 <gl.paschoal3@gmail.com>
2019-10-08 15:29:13 -0300

    teste

--- trabprog3-jordana/Produtor.hpp
+++ trabprog3-jordana/Produtor.hpp
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Produtor.hpp
- * Author: 2017101996
- *
- * Created on 1 de Outubro de 2019, 14:28
- */
-
 #ifndef PRODUTOR_HPP
 #define PRODUTOR_HPP
 #include "Usuario.h"
--- trabprog3-jordana/Usuario.cpp
+++ trabprog3-jordana/Usuario.cpp
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Usuario.cpp
- * Author: 2017101996
- * 
- * Created on 1 de Outubro de 2019, 09:54
- */
-
 #include "Usuario.h"
 
 string Usuario::getNome(){
